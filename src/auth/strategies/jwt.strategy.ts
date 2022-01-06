33b0ca0ce5acdfc2e95963c198ef28cb78dc6871
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-01-06 12:40:01 -0300

    refactor: change key value on validate jwt strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -15,6 +15,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: IPayload) {
-    return { sub: payload.sub, email: payload.email, name: payload.name };
+    return { id: payload.sub, email: payload.email, name: payload.name };
   }
 }
